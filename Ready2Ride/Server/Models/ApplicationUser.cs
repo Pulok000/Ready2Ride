3cf8f1cbdb2ce8e84b52a6cc1a06972fb9c18539
Pulok Ahmed <pulokahmed07@gmail.com>
2023-01-18 02:19:44 +0600

    web api test

--- Ready2Ride/Server/Models/ApplicationUser.cs
+++ Ready2Ride/Server/Models/ApplicationUser.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Ready2Ride.Shared;
 
 namespace Ready2Ride.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
     }
 }
