e65363b82059a81e66af25e5f6603fb071e010e6
Pulok Ahmed <pulokahmed07@gmail.com>
2023-01-18 23:57:42 +0600

    Identity Manamenet: LoginView Controller added

---
+++ Ready2Ride/Server/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ready2Ride.Server.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
