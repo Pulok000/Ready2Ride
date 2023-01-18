4f7210b6c43af6eea550b3e4b3ca5510cab7ca15
Pulok Ahmed <pulokahmed07@gmail.com>
2023-01-19 00:38:27 +0600

    Identity Management: Adding cdns for boostrapes,js,full calander

--- Ready2Ride/Server/Controllers/AccountController.cs
+++ Ready2Ride/Server/Controllers/AccountController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ready2Ride.Server.Data;
 
 namespace Ready2Ride.Server.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
